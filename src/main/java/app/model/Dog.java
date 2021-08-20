109ab9050d487fa2dbe2794d1335545e7586a404
Shaper0k <Novomoskovsk08082002@gmail.com>
2021-08-20 11:10:08 +0300

    Commit

---
+++ src/main/java/app/model/Dog.java
@@ -0,0 +1,15 @@
+package app.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dog extends Animal {
+
+    @Override
+    public String toString() {
+        return "Im a Dog";
+    }
+
+}
